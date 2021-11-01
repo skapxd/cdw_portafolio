64284f63a158615fc0a9819e957659dca7b86c3b
manuel londoño <mlondono@q10.con>
2021-10-31 20:10:48 -0500

    feat: add search function to search page

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,10 +1,66 @@
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import ArrowIcon, {
     ArrowIconDirection
 } from '../../components/lv_1/Icons/ArrowIcon'
 
 import Style from './Search.module.sass'
+import { ListOfCardPost } from '../../components/lv_3/ListOfCardPost/ListOfCardPost'
+import { getBasicData } from '../../helpers/getBasicData'
+import { CardPostI } from '../../components/lv_3/CardPost/CardPost'
+
+export async function getStaticProps() {
+    const { listPost, listTags, mostSeen, lastPost } = getBasicData()
+
+    return {
+        props: {
+            listPost,
+            listTags,
+            mostSeen,
+            lastPost
+        }
+    }
+}
+
+interface SearchI {
+    listPost: CardPostI[]
+}
+export default function Search(props: SearchI) {
+    const { listPost } = props
+    const closeSearch = useCloseSearch()
+
+    const [post, setPost] = useState(listPost)
+
+    return (
+        <div className={Style.search}>
+            <div className={Style.search_bar}>
+                <ArrowIcon
+                    direction={ArrowIconDirection.Left}
+                    onClick={() => closeSearch()}
+                />
+
+                <input
+                    // data-cy="search_input"
+                    className={Style.search_bar_inputText}
+                    placeholder="Buscar post"
+                    onChange={(event) => {
+                        const value = event.currentTarget.value.toLowerCase()
+
+                        const listTemp = listPost.filter((e) => {
+                            return e.title.toLowerCase().includes(value)
+                        })
+
+                        setPost((e) => listTemp)
+                    }}
+                />
+            </div>
+
+            <div className={Style.search_divider}></div>
+
+            <ListOfCardPost list={post} className={Style.search_listCardPost} />
+        </div>
+    )
+}
 
 const search = 'search'
 
@@ -29,39 +85,3 @@ export const useCloseSearch = () => {
 
     return () => router.back()
 }
-
-export function Search() {
-    const router = useRouter()
-    const closeSearch = useCloseSearch()
-    const [ifCanShowSearch, setIfCanShowSearch] = useState(false)
-
-    useEffect(() => {
-        if (router.asPath.includes(search)) {
-            setIfCanShowSearch(true)
-        } else {
-            setIfCanShowSearch(false)
-        }
-    }, [router.asPath])
-
-    if (!ifCanShowSearch) return <></>
-
-    return (
-        <div className={Style.search}>
-            <div className={Style.search_bar}>
-                <ArrowIcon
-                    direction={ArrowIconDirection.Left}
-                    onClick={() => closeSearch()}
-                />
-
-                <input
-                    // data-cy="search_input"
-                    className={Style.search_bar_inputText}
-                    placeholder="Buscar post"
-                />
-            </div>
-            <div className={Style.search_divider}></div>
-        </div>
-    )
-}
-
-export default Search
