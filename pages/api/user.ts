b081561efb649b9638ed24f3954fcf69d004a483
mLondono <mlondono@q10.com>
2022-09-21 21:44:26 -0500

    fix: add user api

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -10,9 +10,9 @@ export default function handler (
   req: NextApiRequest,
   res: NextApiResponse<User>
 ) {
-  return {
+  return res.json({
     age: 23,
     name: 'Manuel',
     address: 'Envigado'
-  }
+  })
 }
