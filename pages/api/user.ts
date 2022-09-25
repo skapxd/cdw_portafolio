2c736db802a89437e7e41c07a6dc2986656c6c40
mLondono <mlondono@q10.com>
2022-09-24 21:18:22 -0500

    fix: add Access-Control-Allow-Origin, * to api/user

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,18 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors'
+import { runMiddleware } from '#/utils/runMiddleware'
 
 interface User {
+  ig: string;
   age: number;
   name: string;
   address: string;
 }
+const cors = Cors({
+  methods: ['GET']
+})
 
-export default function handler (
+export default async function handler (
   req: NextApiRequest,
   res: NextApiResponse<User>
 ) {
+  // await runMiddleware(req, res, cors)
+
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
   return res.json({
     age: 23,
     name: 'Manuel',
-    address: 'Envigado'
+    address: 'Envigado',
+    ig: 'fegge_'
   })
 }
